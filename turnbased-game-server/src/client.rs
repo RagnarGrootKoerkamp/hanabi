e2e1ad07752c4d12933d11cb4d289fdc46bba579
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-01-10 01:55:24 +0100

    client: fix bugs

--- turnbased-game-server/src/client.rs
+++ turnbased-game-server/src/client.rs
@@ -7,19 +7,21 @@ use tokio_util::codec::{FramedRead, LinesCodec};
 pub async fn start_client<Game: GameT>(address: &str) {
     let (stdin_sink, stdin_stream) = futures_channel::mpsc::unbounded();
 
-    tokio::spawn(read_user_input::<Game>(stdin_sink));
-
     let (ws_stream, _) = connect_async(address).await.expect("Failed to connect");
+
+    tokio::spawn(read_user_input::<Game>(stdin_sink));
 
     let (outgoing, incoming) = ws_stream.split();
     let stdin_to_ws = stdin_stream.map(Ok).forward(outgoing);
 
-    let ws_to_stdout = incoming.for_each(|message| async {
-        if let Err(err) = message {
-            eprintln!("Error: {err}");
-            return;
-        }
-        let msg = message.unwrap();
+    let ws_to_stdout = incoming.for_each(|msg| async {
+        let msg = msg
+            .map_err(|err| {
+                eprintln!("Error: {err}");
+                // Kill the hanging stdin task.
+                std::process::exit(1);
+            })
+            .unwrap();
         if !msg.is_binary() {
             return;
         }
